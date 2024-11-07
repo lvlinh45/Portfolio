1fad62aa89055d915c8c4b3f4b9f302ad516a8ce
Luong Van Linh <luongvanlinh.45@gmail.com>
2024-11-07 10:19:06 +0700

    first commit

---
+++ src/components/Skills/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+import { Skill } from "../../data/constants";
+
+const Skills = () => {
+  return <div>Skills</div>;
+};
+
+export default Skills;
