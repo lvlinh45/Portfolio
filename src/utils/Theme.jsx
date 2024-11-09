e924fbe2f03545be0d69e4fc87a393a04f7187d2
LuongVanLinh <luongvanlinh.45@gmail.com>
2024-11-09 13:14:51 +0700

    style: edit background in lightmode

--- src/utils/Theme.jsx
+++ src/utils/Theme.jsx
@@ -14,6 +14,7 @@ export const darkTheme = {
 export const lightTheme = {
   bg: "#FFFFFF",
   bgLight: "#f0f0f0",
+  card_light: "#fff",
   primary: "#be1adb",
   text_primary: "#111111",
   text_secondary: "#48494a",
